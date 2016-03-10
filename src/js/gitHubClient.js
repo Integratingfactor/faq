3a0d819f8f04e32aa0126c6ff94863f6136d6ed4
Amit Bhadoria <gnulib@gmail.com>
2016-03-10 00:31:17 -0800

    updated search templates to search documentation or issues

--- src/js/gitHubClient.js
+++ src/js/gitHubClient.js
@@ -3,6 +3,7 @@
     app.factory('GithubFaqClient', function($log, $http, $httpParamSerializerJQLike){
         var apiBase='https://api.github.com/search/';
         var codeQuery='code?q=';
+        var issuesQuery='issues?q=';
         var queryInRepo='+repo:';
         function httpReq(method, url, body, onSuccess, onError) {
               // $log.log("using access token:" + IdpClient.getToken());
@@ -23,12 +24,27 @@
         };
         return {
             queryInRepo: function(search, repo, onSuccess, onError) {
-                $log.log("searching for", search, "in repo", repo);
+                $log.log("searching for", search, "in code of", repo);
                 httpReq('GET', apiBase + codeQuery + search + queryInRepo + repo, null, onSuccess, onError);
+            },
+            queryInIssues: function(search, repo, onSuccess, onError) {
+                $log.log("searching for", search, "in issues of", repo);
+                httpReq('GET', apiBase + issuesQuery + search + queryInRepo + repo, null, onSuccess, onError);
             },
             getFaqItem: function(git_url, onSuccess, onError) {
                 $log.log("getting faq item", git_url);
                 httpReq('GET', git_url, null, onSuccess, onError);
+            },
+            getIssueComments: function(comments_url, issue, onSuccess, onError) {
+                $log.log("getting faq item", comments_url);
+                httpReq('GET', comments_url, null, function(data) {
+                  if (data.length == 0) {
+                    data = [{body: 'no comments'}];
+                  }
+                  onSuccess(issue, data);
+                }, function(error) {
+                  onError(issue, error);
+                });
             }
         };
     });
